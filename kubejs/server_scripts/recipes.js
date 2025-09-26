251f1871dffe6f367d780dea0470a7422469ba72
Mendaxx <iriseye.marblemellow@gmail.com>
2025-09-26 00:13:00 -0400

    walnut mulch gristmill recipe uses nml walnuts

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -13,12 +13,65 @@ ServerEvents.recipes(e => {
         C: 'minecraft:honeycomb'
         }
     ),
+
+    // - oven recipes
+    
+    e.custom({
+        type: "cookscollection:baking",
+        ingredients: [
+            {"item": "farmersdelight:wheat_dough"},
+            {"tag": "c:foods/milk"},
+            {"item": "farmersdelight:wheat_dough"},
+            {"item": "farmersdelight:wheat_dough"}
+  ],
+        result: 
+            {id: "farmersdelight:pie_crust", count: 1},
+        cookingtime: 200
+})
+    e.custom({
+        type: "cookscollection:baking_shaped",
+        pattern: [
+          "DMD",
+          " D "
+        ],
+        key: {
+          "D": {
+            "item": "farmersdelight:wheat_dough"
+          },
+          "M": {
+            "tag": "c:foods/milk"
+          }
+        },
+        result: 
+            {id: "farmersdelight:pie_crust", count: 3},
+        cookingtime: 200
+})
+
+    // - gritsmill recipes
+    e.remove({ id: 'bountifulfares:walnut_mulch_from_walnut_milling'})
+    e.custom({
+        type: "bountifulfares:milling",
+        ingredient: 
+        {item: "nomansland:walnuts"},
+        result: 
+        {id: "bountifulfares:walnut_mulch"},
+        result_count: 1
+})
+
+
     //simple recipe changes
+
+    // - vanilla
     e.replaceInput(
          {output: 'minecraft:jukebox' }, 
                     'minecraft:diamond',           
                     'minecraft:amethyst_shard'         
     ),
+
+    // - bountiful fares
+    
+
+    // - etched
     e.replaceInput(
          {output: 'etched:etching_table' }, 
                     'minecraft:diamond',           
@@ -29,6 +82,8 @@ ServerEvents.recipes(e => {
                     'minecraft:copper_ingot',           
                     'minecraft:iron_ingot'         
     ),
+
+    // - minersdelight
     e.replaceInput(
          {output: 'minersdelight:copper_pot' }, 
                     'minecraft:copper_ingot',           
@@ -39,6 +94,17 @@ ServerEvents.recipes(e => {
                     'minecraft:copper_ingot',           
                     'minecraft:brick'         
     ),
+
+    // - farmers delight
+    e.replaceInput(
+         {output: 'farmersdelight:wheat_dough' }, 
+                    'minecraft:wheat',           
+                    'bountifulfares:flour'         
+    ),
+
+
+    // - architect's palette
+
 
     //complex recipe changes
     //e.remove({ id: 'minecraft:note_block' }),
@@ -55,6 +121,11 @@ ServerEvents.recipes(e => {
     //    R: 'minecraft:redstone'
 
     //}),
+    e.remove({ id: 'architects_palette:bread_block'})
+    e.remove({ id: 'minecraft:bread'})
+    e.remove({ id: 'farmersdelight:bread_from_smoking'})
+    
+
 
     e.remove({ id: 'heartstone:heartstone' }),
     e.shaped(
