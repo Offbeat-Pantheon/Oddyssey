5667791693770d1f454331a3d3f050923f7ba00e
Mendaxx <iriseye.marblemellow@gmail.com>
2025-09-26 00:13:45 -0400

    passion fruit seeds + registry.js

---
+++ kubejs/startup_scripts/registry.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', e => {
+  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
+  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
+  e.create('passion_fruit_seeds').texture('oddyssey:item/passion_fruit_seeds')
+})
