38b37f5267da07c40df1cc315ee5ab83b9aa6621
Mendaxx <iriseye.marblemellow@gmail.com>
2025-10-05 23:04:24 -0400

    stuff stuff and more stuff

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -8,17 +8,16 @@ StartupEvents.registry('item', e => {
   e.create('firestarter_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('skywing_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('waterbearer_sigil').texture('oddyssey:item/ethereal_sigil')
-  e.create('frostbite_sigil').texture('oddyssey:item/ethereal_sigil')
-  e.create('quicksilver_sigil').texture('oddyssey:item/ethereal_sigil')
+  e.create('frostbiter_sigil').texture('oddyssey:item/ethereal_sigil')
+  e.create('lightrunner_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('shieldbreaker_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('soul_eater_sigil').texture('oddyssey:item/ethereal_sigil')
-  e.create('shadowcloak_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('unseen_sigil').texture('oddyssey:item/ethereal_sigil')
+  e.create('gilded_monarch_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('weaver_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('lawgiver_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('thunderbolt_sigil').texture('oddyssey:item/ethereal_sigil')
   e.create('bombshell_sigil').texture('oddyssey:item/ethereal_sigil')
-
 
 
 })
